bf974adf8ee5dd337e35612c290d420765ff6609
Tyler Nijmeh <tylernij@gmail.com>
2021-06-19 21:35:58 -0700

    Switch nc_flavor syntax to broader box flavor
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- issh.sh
+++ issh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-LOCAL=false     # Shall we only allow localhost connections?
-PORT=65432      # Port to open with netcat
+LOCAL=false # Shall we only allow localhost connections?
+PORT=65432  # Port to open with netcat
 
 # Display usage for this script
 usage() {
@@ -11,50 +11,47 @@ usage() {
   -p PORT     Port to listen for connections on (default: $PORT)"
 }
 
-# Return which flavor of netcat we have (Busybox or Toybox)
-nc_flavor() {
-  for flavor in busybox toybox
-  do
-    if command -v "$flavor" &> /dev/null
-    then
+# Return which flavor of toolbox we have (busybox or toybox)
+box_flavor() {
+  local flavor
+  for flavor in busybox toybox; do
+    if command -v "$flavor" &>/dev/null; then
       echo "$flavor"
       return
     fi
   done
 }
-flavor="$(nc_flavor)"
+box="$(box_flavor)"
 
 # Parse user arguments
-while getopts ":lhp:" opt
-do
+while getopts ":lhp:" opt; do
   case "$opt" in
-    h)
-      usage
-      exit 0
-      ;;
-    l)
-      LOCAL=true
-      ;;
-    p)
-      PORT="$OPTARG"
-      ;;
-    *)
-      usage
-      exit 1
-      ;;
+  h)
+    usage
+    exit 0
+    ;;
+  l)
+    LOCAL=true
+    ;;
+  p)
+    PORT="$OPTARG"
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
   esac
 done
 
 # Check if we can even support this port
-if ss -l 2> /dev/null | grep -q ":$PORT"
-then
+if ss -l 2>/dev/null | grep -q ":$PORT"; then
   echo "Port is in use: $PORT"
   exit 1
 fi
 
-# Depending on the netcat flavor, we need different arguments
+# Depending on the box flavor, we need different arguments
 NCARGS=()
-case "$flavor" in
+case "$box" in
   busybox)
     NCARGS+=(
       "-l"
@@ -79,6 +76,6 @@ esac
 
 # Execute netcat and fork it
 # shellcheck disable=SC2068
-setsid "$flavor" nc ${NCARGS[@]} sh -c "sh -i 2>&1" &
+setsid "$box" nc ${NCARGS[@]} sh -c "sh -i 2>&1" &
 
 echo -e "Done! Use: \e[1mnc localhost $PORT\e[0m"
