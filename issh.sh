835935bf26cc079a14a23f5931fe703daa6d7440
Tyler Nijmeh <tylernij@gmail.com>
2021-06-19 21:55:00 -0700

    Target toybox
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- issh.sh
+++ issh.sh
@@ -11,17 +11,11 @@ usage() {
   -p PORT     Port to listen for connections on (default: $PORT)"
 }
 
-# Return which flavor of toolbox we have (busybox or toybox)
-box_flavor() {
-  local flavor
-  for flavor in busybox toybox; do
-    if command -v "$flavor" &>/dev/null; then
-      echo "$flavor"
-      return
-    fi
-  done
-}
-box="$(box_flavor)"
+if ! command -v toybox &> /dev/null
+then
+  echo "Missing toybox binary"
+  exit 1
+fi
 
 # Parse user arguments
 while getopts ":lhp:" opt; do
@@ -44,38 +38,21 @@ while getopts ":lhp:" opt; do
 done
 
 # Check if we can even support this port
-if ss -l 2>/dev/null | grep -q ":$PORT"; then
+if toybox netstat -lpn 2> /dev/null | toybox grep 65432 &> /dev/null
+then
   echo "Port is in use: $PORT"
   exit 1
 fi
 
 # Depending on the box flavor, we need different arguments
-NCARGS=()
-case "$box" in
-  busybox)
-    NCARGS+=(
-      "-l"
-      "-k"
-      "-p $PORT"
-    )
-    [[ "$LOCAL" == true ]] && NCARGS+=("-s localhost")
-    NCARGS+=("-e")
-    ;;
-  toybox)
-    NCARGS+=(
-      "-L"
-      "-p $PORT"
-    )
-    [[ "$LOCAL" == true ]] && NCARGS+=("-s localhost")
-    ;;
-  *)
-    echo "Neither Busybox nor Toybox detected"
-    exit 1
-    ;;
-esac
+NCARGS=(
+  "-L"
+  "-p $PORT"
+)
+[[ "$LOCAL" == true ]] && NCARGS+=("-s localhost")
 
 # Execute netcat and fork it
 # shellcheck disable=SC2068
-setsid "$box" nc ${NCARGS[@]} sh -c "sh -i 2>&1" &
+toybox setsid toybox nc ${NCARGS[@]} sh -c "sh -i 2>&1" &
 
 echo -e "Done! Use: \e[1mnc localhost $PORT\e[0m"
